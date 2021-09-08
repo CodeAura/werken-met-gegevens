dffa54f4c43a9e10cf7513aebb13531e6bce8e30
ValidCoder <mikedh.prive@gmail.com>
2021-09-08 11:35:12 +0200

    Python bestanden + 2 foto's van de gegevens

---
+++ feestlunch.py
@@ -0,0 +1,8 @@
+croissantjes = 37
+stokbroodjes = 2
+bedrag = 3
+
+print(croissantjes+stokbroodjes)
+
+print("De Feestlunch kost je bij de bakker 18.80 euro voor 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
+
---
+++ speelhal.py
@@ -0,0 +1,5 @@
+mensen = 4
+VR = 44.44
+
+print(4*44.44) # rekend deze som uit
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro")
